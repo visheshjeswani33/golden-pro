e604d8be31ec19745dbf9ebd73246ae8d15a81f5
visheshjeswani33 <64632774+visheshjeswani33@users.noreply.github.com>
2020-12-18 22:19:39 +0530

    Update Intro.js

--- Intro.js
+++ Intro.js
@@ -14,10 +14,13 @@ class Intro{
   this.title2.html("Learn skills to earn money by investing online...");
   this.title2.position(displayWidth/8, displayHeight/8 + 50);
   this.button.position(displayWidth-70, displayHeight-50);
+  
+  var logo = document.createElement("img");
+  logo.src = "images/logo.png";
+  logo.width = 100;
+  logo.height = 100;
+  document.body.appendChild(logo);
 
  }
-
-
-
 
 }
